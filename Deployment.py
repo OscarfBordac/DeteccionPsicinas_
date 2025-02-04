ad5ef9c54553bdda86ce163a651cf7f626b4e9d4
Oscar Borda <142197337+OscarfBordac@users.noreply.github.com>
2025-02-03 23:11:08 -0500

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -9,7 +9,7 @@ import io
 @st.cache_resource
 def init_roboflow():
     rf = Roboflow(api_key="HMtpnDxMMpTmWHSXsxKV")
-    return rf.workspace("ofbordac").project("piscinas-3dwyx").version(6).model
+    return rf.workspace("ofbordac").project("piscinas-3dwyx").version(7).model
 
 def draw_boxes(image, predictions):
     """Dibuja las cajas de predicción en la imagen"""
